c5911855d3e4c1794598828b69cba78bab2ff12a
Alexandr Lazarev <diedsmiling@gmail.com>
2020-05-01 16:37:33 +0200

    Set up dev flow

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,9 +4,9 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 app.get("/", (_, res) => {
-    res.status(200).send("Hello World!");
+    res.status(200).send("Hello World!!");
 });
 
 app.listen(3000, () => {
-  console.log("Starte server on por 3000");
+  console.log("Started server on por 3000");
 });
